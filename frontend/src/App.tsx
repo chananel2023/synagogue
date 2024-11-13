2884943365b4896c14cb8abec943188f771896d8
baruch yankvtz <baruchyankvtz@baruchs-MacBook-Air.local>
2024-11-13 14:02:08 +0200

    Final changes in signup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import './index.css';
+
 import Login from './pages/login';
 import Signup from './pages/signup';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
